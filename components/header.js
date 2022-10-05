d6ebb43f98b1261d0b88590c9bca4705a3aae0ab
lyelc <lyeclyde@gmail.com>
2022-10-06 01:13:37 +0545

    completed header section

--- components/header.js
+++ components/header.js
@@ -1,19 +1,20 @@
+import { ImFacebook2, ImGithub, ImTwitter } from "react-icons/im";
 
 export default function Header() {
   return (
     <header className="bg-gray-50">
-        <div className="xl:container xl:mx-auto flex flex-col item-center sm:flex-row sm:justify-between text-center py-3">
-            <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-between py-4 sm:py-0">
-                <input className="input-text" type="text" placeholder="Search..."/>
+        <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
+            <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
+                <input type="text" className="input-text"  placeholder="Search..."/>
             </div>
             <div className="shrink w-80 sm:order-2">
-                <a>Design</a>
+                <a className="font-bold uppercase text-3xl">Design</a>
             </div>
             <div className="w-96 order-3 flex justify-center">
                 <div className="flex gap-6">
-                    <a>Facebook</a>
-                    <a>Github</a>
-                    <a>Twitter</a>
+                    <a><ImFacebook2 color="#888888" /></a>
+                    <a><ImGithub color="#888888" /></a>
+                    <a><ImTwitter color="#888888" /></a>
                 </div>
             </div>
         </div>
