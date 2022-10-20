c0e98a24830cd4d0f2febce373d1da2470b98651
lyelc <lyeclyde@gmail.com>
2022-10-20 12:30:27 +0545

    changed header

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default function Header() {
                 <input type="text" className="input-text"  placeholder="Search..."/>
             </div>
             <div className="shrink w-80 sm:order-2">
-                <Link href={"/"}><a className="font-bold uppercase text-3xl">Design</a></Link>
+                <Link href={"/"}><a className="font-bold uppercase text-3xl">Ayle</a></Link>
             </div>
             <div className="w-96 order-3 flex justify-center">
                 <div className="flex gap-6">
