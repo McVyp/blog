21741f654eabde2ba4cfff308bfa5c72bc42ff1e
lyelc <lyeclyde@gmail.com>
2022-10-20 13:45:47 +0545

    added slider

--- components/section1.js
+++ components/section1.js
@@ -1,18 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
 import Author from "./_child/author";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import SwiperCore, {Autoplay} from 'swiper';
 
 export default function Section1() {
+
+    SwiperCore.use([Autoplay])
 
     const bg = {
         background:"url('/assets/images/banner.png') no-repeat",
         backgroundPosition: "right"
     }
+
+   
   return (
     <section className='py-16' style={bg}>
         <div className='container mx-auto md:px-20'>
             <h1 className='font-bold text-4xl pb-12 text-center'>Trending</h1>
-            {Slide()}
+            <Swiper
+            slidesPerView={1}
+            loop={true}
+            autoplay = {{
+                delay: 3000
+            }}
+            >
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            
+            </Swiper>
+            
         </div>
     </section>
 
