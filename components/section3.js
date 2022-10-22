97587e0a23f7baec89642706a1eec6f57710dbf3
lyelc <lyeclyde@gmail.com>
2022-10-22 21:36:05 +0545

    Added changes to section 3

--- components/section3.js
+++ components/section3.js
@@ -46,7 +46,7 @@ function Post() {
                     <Link href={"/"}><a className="text-3xl md:text-4xl font-bold text-gray-800 hover:text-gray-600">All things fall apart when they come too close</a></Link>
                 </div>
                 <p className="text-gray-500 py-3">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
                 <Author />
             </div>
